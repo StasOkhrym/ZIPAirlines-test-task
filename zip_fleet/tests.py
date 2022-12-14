82afd07f92250dea93cd80e3a48be986f75fcca8
Stas Okhrym <s.okhrym@gmail.com>
2022-12-14 14:36:22 +0200

    Added requirements

--- zip_fleet/tests.py
+++ zip_fleet/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from zip_fleet.models import Aircraft
+
+from rest_framework.test import APIClient
+
+
+def sample_aircraft(**params):
+    defaults = {
+        "airline": "TestAirline",
+        "seats": 15,
+        "passengers": 10
+    }
+    defaults.update(params)
+
+    return Aircraft.objects.create(**defaults)
+
+
+class AircraftApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
