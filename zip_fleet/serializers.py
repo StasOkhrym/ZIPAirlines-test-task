9afde8c113557b08703fbfd28cf313c5ae3bc6b3
Stas Okhrym <s.okhrym@gmail.com>
2022-12-14 15:09:20 +0200

    Added Airline model, serializer, viewset

--- zip_fleet/serializers.py
+++ zip_fleet/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from zip_fleet.models import Aircraft
+from zip_fleet.models import Aircraft, Airline
+
+
+class AirlineSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Airline
+        fields = ("id", "name")
 
 
 class AircraftSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Aircraft
         fields = (
@@ -15,22 +21,23 @@ class AircraftSerializer(serializers.ModelSerializer):
             "passengers",
             "fuel_capacity",
             "fuel_consumption",
-            "flight_time"
+            "flight_time",
         )
 
 
 class AircraftCreateSerializer(AircraftSerializer):
-
     def create(self, validated_data) -> Aircraft:
-        aircrafts = Aircraft.objects.filter(airline=validated_data["airline"])
+        aircrafts = Aircraft.objects.filter(airline=validated_data["airline"]).count()
 
-        if aircrafts > 10:
+        if aircrafts > 9:
             raise ValidationError(
-                {
-                    "company_fleet":
-                    "company can have no more than 10 aircrafts"
-                }
+                {"company_fleet": "company can have no more than 10 aircrafts"}
             )
 
-        return super(AircraftCreateSerializer, self).create(validated_data=validated_data)
+        return super(AircraftCreateSerializer, self).create(
+            validated_data=validated_data
+        )
 
+
+class AircraftDetailSerializer(AircraftSerializer):
+    airline = AirlineSerializer(many=False, read_only=True)
--- zip_fleet/tests.py
+++ zip_fleet/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
 from zip_fleet.models import Aircraft
 
 from rest_framework.test import APIClient
+
+from zip_fleet.serializers import AircraftSerializer
+
+AIRCRAFT_URL = reverse("zip_fleet:aircraft-list")
 
 
 def sample_aircraft(**params):
@@ -20,3 +26,14 @@ class AircraftApiTests(TestCase):
     def setUp(self) -> None:
         self.client = APIClient()
 
+    def test_list_aircrafts(self):
+        sample_aircraft()
+        sample_aircraft()
+
+        res = self.client.get(AIRCRAFT_URL)
+
+        movies = Aircraft.objects.all().order_by("id")
+        serializer = AircraftSerializer(movies, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
