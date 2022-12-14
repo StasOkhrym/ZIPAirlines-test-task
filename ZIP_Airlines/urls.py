3435af089dbbca191f8d1eed95260b86f845195f
Stas Okhrym <s.okhrym@gmail.com>
2022-12-14 14:08:47 +0200

    Added urls and view

--- ZIP_Airlines/urls.py
+++ ZIP_Airlines/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include("rest_framework.urls")),
+    path("api/zip_fleet/", include("zip_fleet.urls", namespace="zip_fleet")),
 ]
